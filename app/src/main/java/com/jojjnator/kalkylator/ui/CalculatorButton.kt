f0e0db52d01334986173aee0cbf583d31243f55e
nasvalljohan <j.nasvall95@gmail.com>
2023-02-06 16:22:56 +0100

    Crapculator V2 to be approved

--- app/src/main/java/com/jojjnator/kalkylator/ui/CalculatorButton.kt
+++ app/src/main/java/com/jojjnator/kalkylator/ui/CalculatorButton.kt
@@ -1,34 +1,62 @@
 package com.jojjnator.kalkylator.ui
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.jojjnator.kalkylator.RandomGenerator
 import com.jojjnator.kalkylator.ui.theme.BackgroundColor
 import com.jojjnator.kalkylator.ui.theme.ButtonColor
+import kotlin.math.roundToInt
 
 @Composable
-fun CalculatorButton(
+fun CalculatorNumberButton(
     modifier: Modifier = Modifier,
-    operator: String = "7",
-){
+) {
+
+    val randomColor = RandomGenerator.getRandomColor()
+    val infiniteTransition = rememberInfiniteTransition()
+    val noChillText by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 9f,
+        animationSpec = infiniteRepeatable(
+            tween(100),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .background(randomColor)
+            .border(border = BorderStroke(2.5.dp, color = BackgroundColor))
+            .then(modifier),
+        contentAlignment = Alignment.Center,
+
+        ) {
+        Text(
+            text = noChillText.roundToInt().toString(),
+            fontWeight = FontWeight.Light,
+            style = TextStyle(fontSize = 25.sp)
+        )
+    }
+}
+
+@Composable
+fun CalculatorOperatorButton(
+    modifier: Modifier = Modifier,
+    operator: String = ""
+) {
     Box(
         modifier = Modifier
             .background(ButtonColor)
@@ -36,7 +64,7 @@ fun CalculatorButton(
             .then(modifier),
         contentAlignment = Alignment.Center,
 
-        ){
+        ) {
         Text(
             text = operator,
             fontWeight = FontWeight.Light,
--- app/src/main/java/com/jojjnator/kalkylator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/jojjnator/kalkylator/viewmodel/CalculatorViewModel.kt
@@ -3,6 +3,9 @@ package com.jojjnator.kalkylator.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.jojjnator.kalkylator.CalculatorAction
+import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.random.Random
 
 class CalculatorViewModel : ViewModel() {
 
@@ -12,9 +15,9 @@ class CalculatorViewModel : ViewModel() {
         private set
     var operator = mutableStateOf("")
         private set
-
     var calculatedSum = mutableStateOf(0.0)
         private set
+    var numbers = mutableListOf(1,2,3,4,5,6,7,8,9,0)
 
     fun checkInput(actionFromUI: CalculatorAction) {
         when (actionFromUI) {
@@ -23,27 +26,31 @@ class CalculatorViewModel : ViewModel() {
                 secondInput.value = ""
                 firstInput.value = ""
                 calculatedSum.value = 0.0
+                crapTheLator()
             }
             is CalculatorAction.DoCalculation -> doCalculation(operatorInput = operator.value)
 
             is CalculatorAction.Comma -> addCommaToDigit()
 
             is CalculatorAction.SetNumber -> setNumberFromAction(
-                action = actionFromUI,
-                operatorInput = operator.value
+                operatorInput = operator.value,
+                action = actionFromUI
             )
 
             is CalculatorAction.SetOperator -> operator.value = actionFromUI.operator.stringOP
         }
     }
 
-    private fun setNumberFromAction(action: CalculatorAction.SetNumber, operatorInput: String) {
+    private fun setNumberFromAction(operatorInput: String, action: CalculatorAction.SetNumber) {
         if (operatorInput.isEmpty()) {
             firstInput.value += action.number
         }
         if (operatorInput.isNotEmpty()) {
             secondInput.value += action.number
         }
+    }
+    private fun crapTheLator(){
+        numbers = numbers.shuffled().toMutableList()
     }
 
     private fun addCommaToDigit() {
@@ -69,6 +76,7 @@ class CalculatorViewModel : ViewModel() {
             calculatedSum.value = firstInput.value.toDouble() - secondInput.value.toDouble()
         }
     }
+
 }
 
 enum class Operators(val stringOP: String) {
