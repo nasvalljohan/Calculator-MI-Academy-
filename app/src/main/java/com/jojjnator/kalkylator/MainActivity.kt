5103561153ad6be1a559fe7873a156a54be321f2
nasvalljohan <j.nasvall95@gmail.com>
2023-02-07 10:41:05 +0100

    Dependency Injection done

--- app/src/main/java/com/jojjnator/kalkylator/MainActivity.kt
+++ app/src/main/java/com/jojjnator/kalkylator/MainActivity.kt
@@ -7,12 +7,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.jojjnator.kalkylator.di.appModule
 import com.jojjnator.kalkylator.ui.Calculator
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startKoin{
+            androidLogger()
+            androidContext(this@MainActivity)
+            modules(appModule)
+        }
         setContent {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
