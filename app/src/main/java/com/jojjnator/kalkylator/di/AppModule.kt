0e42d16c5e815c7e51f5112560b8cec3687452d9
Johan Näsvall <j.nasvall95@gmail.com>
2023-02-07 11:05:13 +0100

    Merge pull request #6
    * Removed unused imports and comment

--- app/src/main/java/com/jojjnator/kalkylator/di/AppModule.kt
+++ app/src/main/java/com/jojjnator/kalkylator/di/AppModule.kt
@@ -1,11 +1,9 @@
 package com.jojjnator.kalkylator.di
 
-import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 import com.jojjnator.kalkylator.viewmodel.CalculatorViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 
 val appModule = module {
-    //viewModelOf(::CalculatorViewModel)
     viewModel { CalculatorViewModel() }
 }
