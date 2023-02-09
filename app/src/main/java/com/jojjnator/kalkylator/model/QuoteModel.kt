d8bf51c1212cdb58d038bfde200022d347b46203
nasvalljohan <j.nasvall95@gmail.com>
2023-02-09 09:44:16 +0100

    Add House and Character model into QuoteModel. Minimize amount of files.

--- app/src/main/java/com/jojjnator/kalkylator/model/QuoteModel.kt
+++ app/src/main/java/com/jojjnator/kalkylator/model/QuoteModel.kt
@@ -4,7 +4,19 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class QuoteModel(
-
     val character: Character,
     val sentence: String
+)
+
+@Serializable
+data class House(
+    val name: String,
+    val slug: String
+)
+
+@Serializable
+data class Character(
+    val house: House,
+    val name: String,
+    val slug: String
 )
